bd7f7fbd3d99d3cdc12317e47556ef3ec5a9108b
Aashish Moktan <aashiislama@gmail.com>
2022-03-08 14:46:59 +0545

    Added validation for mobile number

--- index.js
+++ index.js
@@ -22,6 +22,30 @@ function validateFullname(){
     }
 }
 
+function validateMobileNumber(){
+    const mobile = document.getElementById('mobile');
+    if(mobile.value.length  == 0){
+        display_msg('mobile_msg','This field is required !!!','red');
+        return false;
+    }
+    else if(mobile.value.match(/[\D]+/g)){
+        display_msg('mobile_msg','Mobile number must contain digit only !!!','red');
+        return false;
+    }
+    else if(!mobile.value.match(/^[9](8|7)/)){
+        display_msg('mobile_msg','Mobile number must starts with 98 or 97 !!!','red');
+        return false;
+    }
+    else if(mobile.value.length != 10){
+        display_msg('mobile_msg','Mobile number must be of length 10 !!!','red');
+        return false;
+    }
+    else {
+        display_msg('mobile_msg','','');
+        return true;
+    }
+}
+
 
 function display_msg(element, msg, color){
     const msgBox = document.getElementById(element);
